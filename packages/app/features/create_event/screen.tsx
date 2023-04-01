f0f87e4641623fc679d41d5346b6a6e85fb5871e
Luc Charier <luc@forgotten.name>
2023-04-01 06:47:17 -0400

    Added startTime and endTime selection to create event

--- packages/app/features/create_event/screen.tsx
+++ packages/app/features/create_event/screen.tsx
@@ -1,6 +1,7 @@
-import { Anchor, Button, H1, H4, Input,TextArea,Paragraph, Separator, Sheet, XStack, YStack } from '@my/ui'
-import React, { useState } from 'react'
+import { Anchor, Button, H1, H4, Input,TextArea,Paragraph, Separator, Sheet, XStack, YStack, Slider, H3 } from '@my/ui'
+import React, { useState, useRef } from 'react'
 import { useLink } from 'solito/link'
+import DatePicker from '@react-native-community/datetimepicker';
 
 import { Navbar } from 'app/components/Navbar'
 
@@ -34,25 +35,65 @@ const Screen1 = ({ setScreen, formData, setFormData }) => {
 
 const Screen2 = ({ setScreen, formData, setFormData }) => {
   const [numPeople, setNumPeople] = useState(0)
-  const [startTime, setStartTime] = useState('')
-  const [endTime, setEndTime] = useState('')
+  // const numPeople = useRef(0);
+  const [startTime, setStartTime] = useState(new Date())
+  const [endTime, setEndTime] = useState(new Date())
+
+  const [startTimeOpen, setStartTimeOpen] = useState(false)
+  const [endTimeOpen, setEndTimeOpen] = useState(false)
 
   const handelNext = () => {
     setFormData({ ...formData, numPeople, startTime, endTime})
     setScreen(3)
   }
 
+  const handleNumPeopleChange = (value) => {
+    // numPeople.current = value;
+    setNumPeople(value)
+  };
+
   return (
     <>
     <YStack bg='$background3' f={1} p="$4" space>
       <H4 color='$text1' ta="left">With how many people?</H4>
-      <Input size="$4"></Input>
+      <H4 color='$text1' ta="left">{numPeople}</H4>
+      <Slider defaultValue={[50]} max={100} step={1} onValueChange={handleNumPeopleChange}>
+        <Slider.Track>
+          <Slider.TrackActive />
+        </Slider.Track>
+        <Slider.Thumb index={0} circular elevate />
+      </Slider>
 
-      <H4 color='$text1' ta="left">Hangout description</H4>
-      <TextArea mih={140} placeholder="Enter your details..." numberOfLines={4} />
+      <H4 color='$text1' ta="left">Start Time</H4>
+      <DatePicker
+        modal
+        mode='datetime'
+        open={startTimeOpen}
+        value={startTime}
+        onConfirm={(startTime) => {
+          setStartTimeOpen(false)
+          setStartTime(startTime)
+        }}
+        onCancel={() => {
+          setStartTimeOpen(false)
+        }}
+      />
+
+      <H4 color='$text1' ta="left">End Time</H4>
+      <DatePicker
+        modal
+        mode='datetime'
+        open={startTimeOpen}
+        value={startTime}
+        onConfirm={(startTime) => {
+          setStartTimeOpen(false)
+          setStartTime(startTime)
+        }}
+        onCancel={() => {
+          setStartTimeOpen(false)
+        }}
+      />
       
-      <H4 color='$text1' ta="left">Select tags</H4>
-
       <Button size="$2" bg='$highlight1' onPress={handelNext}>Next</Button>
     </YStack>
     </>
